7ce72d8971717ab7f7cf65fa0a66890aefeadfe8
atsu0131 <atsushi.isai131@gmail.com>
2022-06-05 21:16:16 +0900

    rspec

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -7,15 +7,7 @@ RSpec.describe FoodEnquete, type: :model do
                             満足度:良い score: 3, 
                             希望するプレゼント:ビール飲み放題 present_id: 1)' do
 
-        enquete = FoodEnquete.new(
-          name: '田中 太郎',
-          mail: 'taro.tanaka@example.com',
-          age: 25,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 1
-        )
+        enquete = FactoryBot.build(:food_enquete_tanaka)
 
         # [Point.3-3-2]「バリデーションが正常に通ること(バリデーションエラーが無いこと)」を検証します。
         expect(enquete).to be_valid
@@ -75,15 +67,7 @@ RSpec.describe FoodEnquete, type: :model do
     context '年齢を確認すること' do
       it '未成年はビール飲み放題を選択できないこと' do
         # [Point.3-5-3]未成年のテストデータを作成します。
-        enquete_sato = FoodEnquete.new(
-          name: '佐藤 仁美',
-          mail: 'hitomi.sato@example.com',
-          age: 19,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 1   # ビール飲み放題
-        )
+        enquete_sato = FactoryBot.build(:food_enquete_sato)
 
         expect(enquete_sato).not_to be_valid
         # [Point.3-5-4]成人のみ選択できる旨のメッセージが含まれることを検証します。
@@ -92,15 +76,7 @@ RSpec.describe FoodEnquete, type: :model do
 
       it '成人はビール飲み放題を選択できないこと' do
         # [Point.3-5-5]未成年のテストデータを作成します。
-        enquete_sato = FoodEnquete.new(
-          name: '佐藤 仁美',
-          mail: 'hitomi.sato@example.com',
-          age: 20,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 1   # ビール飲み放題
-        )
+        enquete_sato = FactoryBot.build(:food_enquete_sato, age: 20)
 
         # [Point.3-5-6]「バリデーションが正常に通ること(バリデーションエラーが無いこと)」を検証します。
         expect(enquete_sato).to be_valid
@@ -109,27 +85,10 @@ RSpec.describe FoodEnquete, type: :model do
     context 'メールアドレスを確認すること' do
       it '同じメールアドレスで再び回答できないこと' do
         # [Point.3-6-1]1つ目のテストデータを作成します。
-        enquete_tanaka = FoodEnquete.new(
-          name: '田中 太郎',
-          mail: 'taro.tanaka@example.com',
-          age: 25,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 1
-        )
-        enquete_tanaka.save
-  
+        FactoryBot.create(:food_enquete_tanaka)
+
         # [Point.3-6-2]2つ目のテストデータを作成します。
-        re_enquete_tanaka = FoodEnquete.new(
-          name: '田中 太郎',
-          mail: 'taro.tanaka@example.com',
-          age: 25,
-          food_id: 0,
-          score: 1,
-          request: 'スープがぬるかった',
-          present_id: 0
-        )
+        re_enquete_tanaka = FactoryBot.build(:food_enquete_tanaka, food_id: 0, score: 1, present_id: 0, request: "スープがぬるかった")
         expect(re_enquete_tanaka).not_to be_valid
 
         # [Point.3-6-3]メールアドレスが既に存在するメッセージが含まれることを検証します。
